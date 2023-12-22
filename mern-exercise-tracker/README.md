327bb63a2cfe8bbbbdc4071c51e9e9dce254ec13
Frank Buono <fbuonojr@fordham.edu>
2023-12-21 19:36:54 -0500

    done

--- mern-exercise-tracker/README.md
+++ mern-exercise-tracker/README.md
@@ -1,4 +1,4 @@
-<!-- youtube video https://www.youtube.com/watch?v=7CqJlxBYj-M&list=PLE--uVVDmBfcY-dDyaNPITPRBVbZkxE2K&index=11 53:01 -->
+<!-- youtube video https://www.youtube.com/watch?v=7CqJlxBYj-M&list=PLE--uVVDmBfcY-dDyaNPITPRBVbZkxE2K&index=11 55:39 -->
 
 <!-- # Getting Started with Create React App
 
--- mern-exercise-tracker/src/components/navbar.component.js
+++ mern-exercise-tracker/src/components/navbar.component.js
@@ -5,7 +5,10 @@ export default class Navbar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                
+                <Link to="/" className="navbar-brand">ExcerTracker</Link>
+                <div className="collapse navbar-collapse">
+                    
+                </div>
             </nav>
         )
     }
